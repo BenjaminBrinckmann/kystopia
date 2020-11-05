55b4b03ebe6e70ca547b7e297494026679da76e4
BenjaminBrinckmann <benjamin@brinckmann.no>
2020-11-05 11:04:45 +0100

    Add JS and events

--- devpush.sh
+++ devpush.sh
@@ -7,6 +7,6 @@ sudo rm -R $sourceDir
 mkdir $sourceDir
 echo "User-agent: * Disallow: /" > $sourceDir/robots.txt
 hugo --environment development
-find $sourceDir -type f -name "*.html" -exec tidy -m -utf8 -q -access 3 --output-html yes --hide-comments yes --show-filename yes -i --vertical-space yes --wrap 80 {} \;
+# find $sourceDir -type f -name "*.html" -exec tidy -m -utf8 -q -access 3 --output-html yes --hide-comments yes --show-filename yes -i --vertical-space yes --wrap 80 {} \;
 
 lftp -c "open ftp://$ftpUser:$ftpPassword@$ftpHost:21; mirror -eRv $sourceDir $destDir; quit;"
---
+++ static/js/videokontroll.js
@@ -0,0 +1,13 @@
+var myVideo = document.getElementById("videowelkom");
+function playVideo() {
+    myVideo.play();
+} 
+
+var myVideo1 = document.getElementById("videofeil");
+function playVideo1() {
+    myVideo1.play();
+}
+var myVideo2 = document.getElementById("videoriktig");
+function playVideo2() {
+    myVideo2.play();
+}
